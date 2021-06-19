234fa6c90742dc804cc50b36a31ada76a043d9c3
kirubel demelah <kiztopia.2020@gmail.com>
2021-06-19 05:41:47 -0400

    reausing componenets

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 
 function App() {
-  var name = {name: "kira", age: 3}
+
   return (
     <div className="App">
       <Navbar/>
       <Home />
-      <p>{name.name}</p>
     </div>
     
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,16 @@ import './Navbar'
 const Home = () =>{
     const [blogs, setBlogs] = useState([
         {title: 'machine learning', body:'are machines are reallyin going to take our jobs or its a fake so p', id:1},
-        {title: 'ai', body:'are machines are reallyin going to take our jobs or its a fake so p', id:2}
+        {title: 'the school system', body:'are machines are reallyin going to take our jobs or its a fake so p', id:2},
+        {title: 'alburt code', body:'are machines are reallyin going to take our jobs or its a fake so p', id:3},
+        {title: 'ai', body:'are machines are reallyin going to take our jobs or its a fake so p', id:4}
     ]);
     return (
         <div className="home">
             <h1>home</h1>
-            <BlogList blogs={blogs} />
+            <BlogList blogs={blogs} title='allblogs '/>
+            second blog
+            <BlogList blogs={blogs.filter((blog) => blog.id == 2)} title='blog 2 '/>
         </div>
         
     )
